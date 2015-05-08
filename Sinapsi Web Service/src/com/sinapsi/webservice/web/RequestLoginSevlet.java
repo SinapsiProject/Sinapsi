558f0885934a48851ca2c4d00b56d84b2b5df4ab
Ayoub Ouarrak <andreyvondorcon@gmail.com>
2015-05-08 18:03:35 +0200

    BGP updated to version 1.3, fixes in request login servlet, added keys manager class that manipulate keys in the db

--- Sinapsi Web Service/src/com/sinapsi/webservice/web/RequestLoginSevlet.java
+++ Sinapsi Web Service/src/com/sinapsi/webservice/web/RequestLoginSevlet.java
@@ -2,12 +2,25 @@ package com.sinapsi.webservice.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.PublicKey;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.tribes.group.interceptors.TwoPhaseCommitInterceptor.MapEntry;
+
+import com.bgp.encryption.Encrypt;
 import com.bgp.generator.KeyGenerator;
+import com.bgp.keymanager.PrivateKeyManager;
+import com.bgp.keymanager.PublicKeyManager;
+import com.bgp.keymanager.SessionKeyManager;
+import com.google.gson.Gson;
+import com.sinapsi.utils.HashMapBuilder;
+import com.sinapsi.webservice.db.KeysManager;
 
 /**
  * Servlet called when a user want to login
@@ -22,12 +35,29 @@ public class RequestLoginSevlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
         response.setContentType("application/json");
+        Gson gson = new Gson();
+        KeysManager keysManager = new KeysManager();
+        // generate local public/private keys
+        KeyGenerator generator = new KeyGenerator();
         
         try {
         	 String email = request.getParameter("email");
-             KeyGenerator generator = new KeyGenerator();
-             //TODO: save key in the db, every user has his own key
-             //TODO: send public key to the user
+        	 String clientPublicKey = request.getParameter("pub");
+        	 String clientSessionKey = request.getParameter("skey");
+        	 
+        	 Encrypt encrypt = new Encrypt(PublicKeyManager.convertToKey(clientPublicKey));
+        	 String localPrivateKey = PrivateKeyManager.convertToString(generator.getPrivateKey());
+        	 String localPublicKey = PublicKeyManager.convertToString(generator.getPublicKey());
+        	 String localSessionKey = SessionKeyManager.convertToString(encrypt.getEncryptedSessionKey());
+        	 
+        	 // update user keys in the db
+        	 keysManager.updateRemoteKeys(email, clientPublicKey, clientSessionKey);
+        	 // update local keys 
+        	 keysManager.updateLocalKeys(email, localPublicKey, localPrivateKey, localSessionKey);
+        	 
+        	 //send local public key and session key to the client
+        	 out.print(gson.toJson(new HashMap.SimpleEntry<String, String>(localPublicKey, localSessionKey)));
+             out.flush();
              
         } catch(Exception e) {
         	e.printStackTrace();
