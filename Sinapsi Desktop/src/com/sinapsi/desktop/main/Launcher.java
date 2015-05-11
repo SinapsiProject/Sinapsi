e3c72ab8aa5f627d0d4a2b9e351982f7f29e55e0
MarcoGrillo <mark0s_01@hotmail.com>
2015-05-11 15:01:58 +0200

    Loaded and modified the class Launcher.java

--- Sinapsi Desktop/src/com/sinapsi/desktop/main/Launcher.java
+++ Sinapsi Desktop/src/com/sinapsi/desktop/main/Launcher.java
@@ -1,16 +1,94 @@
+/**
+ * @author Marco Grillo
+ */
+
 package com.sinapsi.desktop.main;
 
+import java.awt.image.ImagingOpException;
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
+	
+	private Stage primaryStage;
+	private BorderPane rootLayout;
 
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Sinapsi");
 		
+		initRootLayout();
+		initLoginLayout();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	
+	// Root layout (main layout) loader
+	public void initRootLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Launcher.class.getResource("..view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			// Showing the scene containing the root Layout
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	// Login layout loader
+	public void initLoginLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Launcher.class.getResource("../view/LoginLayout.fxml"));
+			AnchorPane loginLayout = (AnchorPane) loader.load();
+			
+			// Sets the login layout into the center of the root layout
+			rootLayout.setCenter(loginLayout);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Registration layout loader
+	public void initRegistrationLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Launcher.class.getResource("../view/RegistrationLayout.fxml"));
+			AnchorPane registrationLayout = (AnchorPane) loader.load();
+			
+			// Sets the registration layout into the center of the root layout if the register button is pressed
+			rootLayout.setCenter(registrationLayout);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Macro editor layout loader
+	public void initMacroEditorLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Launcher.class.getResource("../view/MacroEditorLayout.fxml"));
+			AnchorPane macroEditorLayout = (AnchorPane) loader.load();
+			
+			// Sets the macro editor layout into the center of the root layout after login has been verified
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
