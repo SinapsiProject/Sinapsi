2cb396d59dee0e4c19733c94d85e54630487a5ef
MarcoGrillo <mark0s_01@hotmail.com>
2015-05-11 17:39:29 +0200

    Some changes to Login Controller(again)

--- Sinapsi Desktop/src/com/sinapsi/desktop/view/LoginLayoutController.java
+++ Sinapsi Desktop/src/com/sinapsi/desktop/view/LoginLayoutController.java
@@ -10,32 +10,4 @@ import javafx.scene.image.Image;
 
 public class LoginLayoutController {
 	
-	
-	// Reference to the launcher
-	private Launcher launcher;
-	
-	/**
-	 * - Constructor -
-	 * The Constructor is called before the initialize() method 
-	 */
-	public LoginLayoutController() {
-		
-	}
-	
-	/**
-	 * Initializes the controller class. This method is automatically called 
-	 * after the fxml has been loaded.
-	 */
-	@FXML 
-	private void initialize() {
-		
-	}
-	
-	/**
-	 * This is called by the main application to give a reference back to itself
-	 * @param launcher
-	 */
-	public void setLauncher(Launcher launcher) {
-		this.launcher = launcher;
-	}
 }
