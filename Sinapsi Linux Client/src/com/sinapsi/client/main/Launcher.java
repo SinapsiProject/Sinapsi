93ce31e72dcfbcda861fa2edc3065d4b136f661e
MarcoGrillo <mark0s_01@hotmail.com>
2015-05-11 10:11:37 +0200

    Added scheme of Linux Client

---
+++ Sinapsi Linux Client/src/com/sinapsi/client/main/Launcher.java
@@ -0,0 +1,16 @@
+package com.sinapsi.client.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
