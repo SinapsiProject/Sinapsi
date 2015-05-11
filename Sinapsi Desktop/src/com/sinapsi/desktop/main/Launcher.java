4790802b9d347493fce80e898fdd56fba36ddf35
MarcoGrillo <mark0s_01@hotmail.com>
2015-05-11 17:32:35 +0200

    Gave a look to the login layout

--- Sinapsi Desktop/src/com/sinapsi/desktop/main/Launcher.java
+++ Sinapsi Desktop/src/com/sinapsi/desktop/main/Launcher.java
@@ -4,15 +4,18 @@
 
 package com.sinapsi.desktop.main;
 
-import java.awt.image.ImagingOpException;
+
 import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
@@ -24,7 +27,8 @@ public class Launcher extends Application {
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Sinapsi");
-		
+		this.primaryStage.setResizable(false);
+			
 		initRootLayout();
 		initLoginLayout();
 	}
@@ -38,7 +42,7 @@ public class Launcher extends Application {
 	public void initRootLayout() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(Launcher.class.getResource("..view/RootLayout.fxml"));
+			loader.setLocation(Launcher.class.getResource("../view/RootLayout.fxml"));
 			rootLayout = (BorderPane) loader.load();
 			
 			// Showing the scene containing the root Layout
@@ -58,6 +62,14 @@ public class Launcher extends Application {
 			loader.setLocation(Launcher.class.getResource("../view/LoginLayout.fxml"));
 			AnchorPane loginLayout = (AnchorPane) loader.load();
 			
+			HBox horizontalBox = new HBox();
+			horizontalBox.setPrefWidth(600);
+			horizontalBox.setPrefHeight(300);
+			
+			Image img = new Image(Launcher.class.getResourceAsStream("../view/logo.png"), 700, 400, true, true);
+			ImageView imgView = new ImageView(img);			
+	        horizontalBox.getChildren().add(imgView);
+		
 			// Sets the login layout into the center of the root layout
 			rootLayout.setCenter(loginLayout);
 		} catch(IOException e) {
