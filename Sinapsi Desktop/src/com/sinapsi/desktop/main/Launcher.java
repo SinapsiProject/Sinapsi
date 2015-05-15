6a29419f23037ab28cd8ec3cd99efda2b4196a4f
MarcoGrillo <mark0s_01@hotmail.com>
2015-05-15 10:35:33 +0200

    Changes

--- Sinapsi Desktop/src/com/sinapsi/desktop/main/Launcher.java
+++ Sinapsi Desktop/src/com/sinapsi/desktop/main/Launcher.java
@@ -9,16 +9,43 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
 	
 	private Stage primaryStage;
 	// private Stage secondaryStage;
+	
+	// Panes
 	private BorderPane root;
-
+	private AnchorPane loginLayout;
+	private GridPane gridPane;
+	
+	// Buttons
+	private Button signIn;
+	
+	// Labels
+	private Label emailLabel;
+	private Label passwordLabel;
+	private Label registerLabel;
+	private Label copyrightLabel;
+	private Label aboutLabel;
+	
+	// TextFields
+	private TextField emailField;
+	private PasswordField passwordField;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
@@ -34,7 +61,34 @@ public class Launcher extends Application {
 	
 	public void initRootLayout() {
 		this.root = new BorderPane();
-		Scene firstScene = new Scene(root, 800, 600);
+		this.loginLayout = new AnchorPane();
+		Scene firstScene = new Scene(root, 800, 600,Color.GRAY);
+		
+//		Image logo = new Image(getClass().getResourceAsStream("../res/rsz_31logo.png"));
+//		ImageView logoView = new ImageView();
+//		logoView.setImage(logo);
+//      root.setTop(logoView);
+		
+		emailLabel = new Label("Email");
+		emailField = new TextField("Insert email...");
+		
+		passwordLabel = new Label("Password");
+		passwordField = new PasswordField();
+		
+		gridPane = new GridPane();
+		gridPane.add(emailLabel, 0, 0);
+		gridPane.add(emailField, 0, 1);
+		gridPane.add(passwordLabel, 0, 2);
+		gridPane.add(passwordField, 0, 3);
+		
+		root.getChildren().add(gridPane);
+		root.setCenter(gridPane);
+		
+		
+		
+		
+		
+		
 		primaryStage.setScene(firstScene);
 		primaryStage.show();
 	}
