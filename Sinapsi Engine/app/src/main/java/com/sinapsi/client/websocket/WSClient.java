520813172c6bcd8bf2aa2f21a5abe2b182de504c
andreyvondorcon <andreyvondorcon@gmail.com>
2015-06-11 17:08:46 +0200

    Added new contructor for websocket client

--- Sinapsi Engine/app/src/main/java/com/sinapsi/client/websocket/WSClient.java
+++ Sinapsi Engine/app/src/main/java/com/sinapsi/client/websocket/WSClient.java
@@ -9,21 +9,31 @@ import com.sinapsi.client.AppConsts;
  * Web Socket Client
  */
 public class WSClient extends WebSocketClient {
-
+    private String username;
+    
     /**
-     * Defaut Ctor.
-     * @throws URISyntaxException
+     * Default Ctor
      */
     public WSClient() throws URISyntaxException {
         super(new URI(AppConsts.SINAPSI_WS_URL));
+    }
+    
+    /**
+     * Secondary Ctor.
+     * @throws URISyntaxException
+     */
+    public WSClient(String username) throws URISyntaxException {
+        super(new URI(AppConsts.SINAPSI_WS_URL), username);
+        this.username = username;
     }
 
     /**
      * Secondary Ctor
      * @param serverURI uri of the
      */
-    public WSClient(URI serverURI) {
-        super(serverURI);
+    public WSClient(URI serverURI, String username) {
+        super(serverURI, username);
+        this.username = username;
     }
 
     /**
@@ -35,27 +45,49 @@ public class WSClient extends WebSocketClient {
         System.out.println("Connection open :" + this.getURI());
     }
 
+    /**
+     * Method called whene messagge is recived
+     */
     @Override
     public void onMessage(String message) {
         //Override this to handle messages
     }
 
+    /**
+     * Method called when the connetion closed
+     */
     @Override
     public void onClose(int code, String reason, boolean remote) {
         System.err.println("You have been disconnected from: " + getURI() + "; Code: " + code + " " + reason);
     }
 
+    /**
+     * Method called on comunication error
+     */
     @Override
     public void onError(Exception ex) {
         ex.printStackTrace();
     }
 
+    /**
+     * Establish connection with the server websocket
+     */
     public void establishConnection() {
         this.connect();
     }
 
+    /**
+     * Close connection with the server websocket
+     */
     public void closeConnection() {
         this.close();
     }
 
+    /**
+     * Return the username of the client
+     * @return
+     */
+    public String getusername() {
+        return this.username;
+    }
 }
