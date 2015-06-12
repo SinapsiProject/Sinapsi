79ae3645a32124748b1a3dab0dd7948216aadf9b
ParsleyJ <parsleyjoe@gmail.com>
2015-06-12 10:18:31 +0200

    implemented class Pair

--- Sinapsi Engine/app/src/main/java/com/sinapsi/utils/Pair.java
+++ Sinapsi Engine/app/src/main/java/com/sinapsi/utils/Pair.java
@@ -1,7 +1,26 @@
 package com.sinapsi.utils;
 
+import com.sinapsi.model.impl.ComunicationInfo;
+
 /**
- * Created by Giuseppe on 11/05/15.
+ * Class used to serialize a pair of object across Sinapsi's network
  */
-public class Pair {
+public class Pair<T1, T2> extends ComunicationInfo{
+
+    private T1 first = null;
+    private T2 second = null;
+
+    public Pair(T1 first, T2 second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public T1 getFirst(){
+        return first;
+    }
+
+    public T2 getSecond(){
+        return second;
+    }
+
 }
