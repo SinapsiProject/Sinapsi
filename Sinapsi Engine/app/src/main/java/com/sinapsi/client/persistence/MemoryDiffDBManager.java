76e81396883254f753392f45596738bde30ee49b
ParsleyJ <parsleyjoe@gmail.com>
2015-06-15 14:51:49 +0200

    added MemoryDiffDBManager

---
+++ Sinapsi Engine/app/src/main/java/com/sinapsi/client/persistence/MemoryDiffDBManager.java
@@ -0,0 +1,42 @@
+package com.sinapsi.client.persistence;
+
+import com.sinapsi.client.persistence.syncmodel.MacroChange;
+import com.sinapsi.model.MacroInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Multi-platform implementation of DiffDBManager, which keeps all the changes
+ * in an ArrayList in memory, instead of saving data to a persistent db
+ * (used to make a diff between two macro DBs).
+ */
+public class MemoryDiffDBManager implements DiffDBManager {
+
+    List<MacroChange> changes = new ArrayList<>();
+
+    @Override
+    public void macroAdded(MacroInterface macro) {
+        changes.add(new MacroChange(MacroChange.ChangeTypes.ADDED, macro.getId()));
+    }
+
+    @Override
+    public void macroUpdated(MacroInterface macro) {
+        changes.add(new MacroChange(MacroChange.ChangeTypes.EDITED, macro.getId()));
+    }
+
+    @Override
+    public void macroRemoved(int id) {
+        changes.add(new MacroChange(MacroChange.ChangeTypes.REMOVED, id));
+    }
+
+    @Override
+    public List<MacroChange> getAllChanges() {
+        return changes;
+    }
+
+    @Override
+    public void clearDB() {
+        changes.clear();
+    }
+}
