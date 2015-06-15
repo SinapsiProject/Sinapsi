57b54681c6068300ff96ab3e83290c0cd5300a6a
ParsleyJ <parsleyjoe@gmail.com>
2015-06-15 19:30:07 +0200

    added Triplet class

---
+++ Sinapsi Engine/app/src/main/java/com/sinapsi/utils/Triplet.java
@@ -0,0 +1,28 @@
+package com.sinapsi.utils;
+
+/**
+ * Class used to serialize a triplet of object across Sinapsi's network
+ */
+public class Triplet<T1,T2,T3> {
+    T1 first;
+    T2 second;
+    T3 third;
+
+    public Triplet(T1 first, T2 second, T3 third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    public T3 getThird() {
+        return third;
+    }
+}
