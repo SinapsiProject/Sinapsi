56c875582fb3f9c05cfa1943e20a804b0abf2678
MarcoGrillo <mark0s_01@hotmail.com>
2015-06-29 10:50:12 +0200

    update on RetrofiWebServiceFacade

---
+++ Sinapsi Desktop/src/com/sinapsi/desktop/persistence/DesktopDiffDBManager.java
@@ -0,0 +1,41 @@
+package com.sinapsi.desktop.persistence;
+
+import java.util.List;
+
+import com.sinapsi.client.persistence.DiffDBManager;
+import com.sinapsi.client.persistence.syncmodel.MacroChange;
+import com.sinapsi.model.MacroInterface;
+
+public class DesktopDiffDBManager implements DiffDBManager {
+
+	@Override
+	public void macroAdded(MacroInterface macro) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void macroUpdated(MacroInterface macro) {
+		// TODO SQLite
+		
+	}
+
+	@Override
+	public void macroRemoved(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearDB() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<MacroChange> getAllChanges() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
