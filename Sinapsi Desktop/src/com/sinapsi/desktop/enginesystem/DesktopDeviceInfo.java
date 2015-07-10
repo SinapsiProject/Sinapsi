40f9842e8fe2373523df4abc46f6f76fc4f6a2dc
MarcoGrillo <mark0s_01@hotmail.com>
2015-07-10 15:51:59 +0200

    Activated notification action from table to dekstop(linux)

--- Sinapsi Desktop/src/com/sinapsi/desktop/enginesystem/DesktopDeviceInfo.java
+++ Sinapsi Desktop/src/com/sinapsi/desktop/enginesystem/DesktopDeviceInfo.java
@@ -1,31 +1,29 @@
 package com.sinapsi.desktop.enginesystem;
 
-import java.io.IOException;
-
 import com.sinapsi.desktop.DesktopConsts;
 import com.sinapsi.desktop.controller.RootAccess;
 import com.sinapsi.engine.system.DeviceInfoAdapter;
 
 public class DesktopDeviceInfo implements DeviceInfoAdapter {
-	
+
 	private String rootPsw;
-	
+
 	public DesktopDeviceInfo(String rootPsw) {
 		this.rootPsw = rootPsw;
 	}
-	
+
 	@Override
 	public String getDeviceName() {
 		// execute command with root privilege
 		String out = null;
 		try {
-		Process p = RootAccess.runFromRoot("hdparm -I /dev/sd? | grep 'Serial\\ Number'", rootPsw);
-		out = RootAccess.streamToString(p.getInputStream());
-		out = out.substring(out.indexOf(":") + 1, out.indexOf("\n"));
+			Process p = RootAccess.runFromRoot("hdparm -I /dev/sd? | grep 'Serial\\ Number'", rootPsw);
+			out = RootAccess.streamToString(p.getInputStream());
+			out = out.substring(out.indexOf(":") + 1, out.indexOf("\n"));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return out;
 	}
 
@@ -34,13 +32,13 @@ public class DesktopDeviceInfo implements DeviceInfoAdapter {
 		// execute command with root privilege
 		String out = null;
 		try {
-		Process p = RootAccess.runFromRoot("dmidecode | grep \"Manufacturer\"", rootPsw);
-		out = RootAccess.streamToString(p.getInputStream());
-		out = out.substring(out.indexOf(":") + 1, out.indexOf("\n"));
+			Process p = RootAccess.runFromRoot("dmidecode | grep \"Manufacturer\"", rootPsw);
+			out = RootAccess.streamToString(p.getInputStream());
+			out = out.substring(out.indexOf(":") + 1, out.indexOf("\n"));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return out;
 	}
 
