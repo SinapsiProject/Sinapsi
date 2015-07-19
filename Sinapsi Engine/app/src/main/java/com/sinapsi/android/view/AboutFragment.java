18154bd07cfbc9d202aad096f925d5441877808b
ParsleyJ <parsleyjoe@gmail.com>
2015-07-19 17:34:29 +0200

    implemented simple about fragment

--- Sinapsi Engine/app/src/main/java/com/sinapsi/android/view/AboutFragment.java
+++ Sinapsi Engine/app/src/main/java/com/sinapsi/android/view/AboutFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.sinapsi.android.background.SinapsiFragment;
 import com.sinapsi.android.R;
@@ -13,6 +15,9 @@ import com.sinapsi.android.R;
  * About fragment with app infos and links.
  */
 public class AboutFragment extends SinapsiFragment {
+    private static final int ANIM_TOTAL_DURATION = 1500;
+    private static final int ANIM_START_OFFSET = 500;
+
     @Override
     public String getName(Context context) {
         return context.getString(R.string.about_fragment_title);
@@ -20,8 +25,29 @@ public class AboutFragment extends SinapsiFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
-        //TODO: impl
+        Animation alpha1 = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_alpha);
+        alpha1.setStartOffset(ANIM_START_OFFSET);
+        alpha1.setDuration(ANIM_TOTAL_DURATION / 3);
+        alpha1.setFillAfter(true);
+
+        Animation alpha2 = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_alpha);
+        alpha1.setFillAfter(true);
+        alpha2.setStartOffset(ANIM_START_OFFSET + alpha1.getDuration());
+        alpha2.setDuration(ANIM_TOTAL_DURATION / 3);
+
+        Animation alpha3 = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_alpha);
+        alpha1.setFillAfter(true);
+        alpha3.setStartOffset(ANIM_START_OFFSET + alpha1.getDuration() + alpha2.getDuration());
+        alpha3.setDuration(ANIM_TOTAL_DURATION / 3);
+
+
+        rootView.findViewById(R.id.logo).startAnimation(alpha1);
+        rootView.findViewById(R.id.brought_label).startAnimation(alpha2);
+        rootView.findViewById(R.id.names_label).startAnimation(alpha3);
+
+
+        return rootView;
     }
 }
